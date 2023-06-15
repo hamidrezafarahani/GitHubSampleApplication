d4636e13ef4f398e61204866a635247959797c09
HamidReza Farahani <h.farahani>
2023-06-15 20:43:53 +0430

    define repo dao and DB class.

---
+++ app/src/main/java/com/example/githubsampleapplication/data/local/DB.kt
@@ -0,0 +1,10 @@
+package com.example.githubsampleapplication.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Repo::class], version = 1, exportSchema = false)
+abstract class DB : RoomDatabase() {
+
+    abstract fun repoDao(): RepoDao
+}
---
+++ app/src/main/java/com/example/githubsampleapplication/data/local/RepoDao.kt
@@ -0,0 +1,25 @@
+package com.example.githubsampleapplication.data.local
+
+import androidx.paging.PagingSource
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface RepoDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(repos: List<Repo>)
+
+    @Query("DELETE FROM repos")
+    suspend fun clearRepos()
+
+    @Query(
+        """
+        SELECT * FROM repos WHERE name LIKE :queryString OR description LIKE :queryString
+        ORDER BY stars DESC, name ASC
+    """
+    )
+    fun reposByName(queryString: String): PagingSource<Int, Repo>
+}
