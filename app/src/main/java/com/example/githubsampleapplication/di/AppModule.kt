54de7419ffdc2ae038e2d2d2c7641373e21b3b99
HamidReza Farahani <h.farahani>
2023-06-15 22:42:59 +0430

    provide DB in app module

--- app/src/main/java/com/example/githubsampleapplication/di/AppModule.kt
+++ app/src/main/java/com/example/githubsampleapplication/di/AppModule.kt
@@ -1,10 +1,30 @@
 package com.example.githubsampleapplication.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.githubsampleapplication.data.local.DB
+import com.example.githubsampleapplication.data.local.RepoDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    fun provideDB(@ApplicationContext context: Context): DB {
+        return Room.databaseBuilder(
+            context,
+            DB::class.java,
+            "github-app.database"
+        ).build()
+    }
+
+    @Provides
+    fun provideRepoDao(db: DB): RepoDao {
+        return db.repoDao()
+    }
 }
