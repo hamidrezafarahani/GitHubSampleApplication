de0b9ae35001549542dfffd81707e2e279253210
HamidReza Farahani <h.farahani>
2023-06-16 19:13:25 +0430

    modified search repos view model

--- app/src/main/java/com/example/githubsampleapplication/ui/SearchReposViewModel.kt
+++ app/src/main/java/com/example/githubsampleapplication/ui/SearchReposViewModel.kt
@@ -2,14 +2,93 @@ package com.example.githubsampleapplication.ui
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.githubsampleapplication.data.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @HiltViewModel
 class SearchReposViewModel @Inject constructor(
     private val repository: Repository,
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
+    val state: StateFlow<UiState>
+    val pagingDataFlow: Flow<PagingData<UiModel>>
+    val accept: (UiAction) -> Unit
+
+    init {
+        val initialQuery = savedStateHandle[LAST_SEARCH_QUERY] ?: DEFAULT_QUERY
+        val lastQueryScrolled = savedStateHandle[LAST_QUERY_SCROLLED] ?: DEFAULT_QUERY
+
+        val actionStateFlow = MutableSharedFlow<UiAction>()
+
+        val searches = actionStateFlow
+            .filterIsInstance<UiAction.Search>()
+            .distinctUntilChanged()
+            .onStart {
+                emit(UiAction.Search(initialQuery))
+            }
+        val queriesScrolled = actionStateFlow
+            .filterIsInstance<UiAction.Scroll>()
+            .distinctUntilChanged()
+            .shareIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000),
+                replay = 1
+            )
+            .onStart {
+                emit(UiAction.Scroll(lastQueryScrolled))
+            }
+
+        state = combine(searches, queriesScrolled, ::Pair)
+            .map { (search, scroll) ->
+                UiState(
+                    query = search.query,
+                    lastQueryScrolled = scroll.currentQuery,
+                    hasNotScrolledForCurrentSearch = search.query != scroll.currentQuery
+                )
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = UiState()
+            )
+
+        pagingDataFlow = searches.flatMapLatest {
+            searchRepos(it.query)
+        }.cachedIn(viewModelScope)
+
+        accept = {
+            viewModelScope.launch {
+                actionStateFlow.emit(it)
+            }
+        }
+    }
+
+    override fun onCleared() {
+        savedStateHandle[LAST_SEARCH_QUERY] = state.value.query
+        savedStateHandle[LAST_QUERY_SCROLLED] = state.value.lastQueryScrolled
+        super.onCleared()
+    }
+
+    private fun searchRepos(query: String): Flow<PagingData<UiModel>> {
+        TODO()
+    }
 }
